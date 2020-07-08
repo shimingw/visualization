d6f0d2bf48d9f2f213cf3f0ca862c0d29859a00d
smw <shimingw@foxmail.com>
2020-07-08 20:07:48 +0800

    first commit

---
+++ canvas/2.js
@@ -0,0 +1,60 @@
+const data = {
+  name: '中国',
+  children: [
+    {
+      name: '江苏',
+      children: [{ name: '南京' }, { name: '宜兴' }, { name: '宜兴' }],
+    },
+    {
+      name: '浙江',
+      children: [
+        { name: '杭州' },
+        { name: '宁波' },
+        { name: '温州' },
+        { name: '绍兴', children: [{ name: 'cun' },{ name: 'cun' }] },
+      ],
+    },
+    {
+      name: '广西',
+      children: [{ name: '桂林' }, { name: '南宁' }],
+    },
+  ],
+}
+
+const regions = d3
+  .hierarchy(data)
+  .sum((d) => 1)
+  .sort((a, b) => b.value - a.value)
+
+const pack = d3.pack().size([512, 512]).padding(3)
+const root = pack(regions)
+
+const canvas = document.querySelector('#canvas')
+const context = canvas.getContext('2d')
+const TAU = 2 * Math.PI
+
+function draw(
+  ctx,
+  node,
+  { fillStyle = 'rgba(0, 0, 0, 0.2)', textColor = 'white' } = {}
+) {
+  const children = node.children
+  const { x, y, r } = node
+  ctx.fillStyle = fillStyle
+  ctx.beginPath()
+  ctx.arc(x, y, r, 0, TAU)
+  ctx.fill()
+  if (children) {
+    for (let i = 0; i < children.length; i++) {
+      draw(ctx, children[i])
+    }
+  } else {
+    const name = node.data.name
+    ctx.fillStyle = textColor
+    ctx.font = '1.5rem Arial'
+    ctx.textAlign = 'center'
+    ctx.fillText(name, x, y)
+  }
+}
+
+draw(context, root)
